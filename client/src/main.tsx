1857e0065810f9b80a4a420227d40850f6210c06
Fexxix <fexxix@gmail.com>
2024-03-19 12:23:33 +0500

    chore: update cuz of prettier format

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AuthContextProvider } from "./contexts/AuthContextProvider.tsx"
 
-const browserRouter = createBrowserRouter([{ path: "/", element: <App /> }])
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
