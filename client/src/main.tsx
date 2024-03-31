c99520f79b92afe768a804097413a4ac276c34a4
Fexxix <fexxix@gmail.com>
2024-03-31 12:10:19 +0500

    add: added HeaderLayout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
 import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AuthContextProvider } from "./contexts/AuthContextProvider.tsx"
+import Home from "./pages/home.jsx"
+import { ThemeProvider } from "./contexts/ThemeProvider.tsx"
+import { Signup } from "./pages/signup.tsx"
+import { HeaderLayout } from "./components/HeaderLayout.tsx"
 
 const browserRouter = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    element: <HeaderLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
   },
 ])
 const queryClient = new QueryClient()
@@ -18,7 +30,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <RouterProvider router={browserRouter} />
+        <ThemeProvider>
+          <RouterProvider router={browserRouter} />
+        </ThemeProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
