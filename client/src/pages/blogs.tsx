aac35a6bf78ad811774ebbf93edea5b0e94332ce
Fexxix <fexxix@gmail.com>
2024-04-03 09:59:10 +0500

    feat: completed /blogs route (for now)

--- client/src/pages/blogs.tsx
+++ client/src/pages/blogs.tsx
@@ -1,3 +1,188 @@
+import { API_URL } from "@/config"
+import { useInfiniteQuery } from "@tanstack/react-query"
+import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Link } from "react-router-dom"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
+import { CiHeart } from "react-icons/ci"
+import { Skeleton } from "@/components/ui/skeleton"
+import { cn } from "@/lib/utils"
+
+type Blog = {
+  id: string
+  title: string
+  description?: string
+  image?: string
+  likes: number
+  author: {
+    username: string
+    profilePicture: string
+  }
+  datePublished: string
+}
+
+type InfiniteBlogList = {
+  blogs: Blog[]
+  hasMore: boolean
+  nextPage: number
+}
+
+const publicDateFormatter = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+})
+
+const likesCountFormatter = new Intl.NumberFormat("en", {
+  notation: "compact",
+  compactDisplay: "short",
+})
+
 export function Blogs() {
-  return <></>
+  const {
+    data,
+    // error,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<InfiniteBlogList>({
+    queryKey: ["blogs"],
+    queryFn: async ({ pageParam }) =>
+      (await axios.get(`${API_URL}/blogs?page=${pageParam}`)).data,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    refetchOnWindowFocus: false,
+  })
+
+  if (!data) {
+    return <InfiniteBlogsSkeleton isInitialLoad />
+  }
+
+  return (
+    <InfiniteScroll
+      dataLength={data.pages.length}
+      hasMore={hasNextPage}
+      next={fetchNextPage}
+      loader={<InfiniteBlogsSkeleton />}
+      className="flex flex-col items-center"
+      children={
+        <div className="flex flex-col justify-center gap-5 max-w-full w-4/5 pt-20 size-full">
+          {data.pages.map((page) =>
+            page.blogs.map((blog) => <BlogCard blogData={blog} key={blog.id} />)
+          )}
+        </div>
+      }
+    />
+  )
+}
+
+export function BlogCard({ blogData }: { blogData: Blog }) {
+  return (
+    <Link
+      to={`/blogs/${blogData.author.username}/${blogData.title
+        .replace(/ /g, "-")
+        .toLocaleLowerCase()}`}
+      className="contents"
+    >
+      <Card>
+        <CardHeader>
+          <div className="flex items-center gap-2 pb-2">
+            <Avatar className="size-8">
+              <AvatarImage
+                className="bg-zinc-800 rounded-full"
+                src={blogData.author.profilePicture}
+              />
+              <AvatarFallback>{blogData.author.username}</AvatarFallback>
+            </Avatar>
+            <span className="text-sm sm:text-base">
+              {blogData.author.username}
+            </span>
+            <div className="size-0.5 bg-black dark:bg-white rounded-full mt-0.5" />
+            <span className="text-xs sm:text-sm">
+              {publicDateFormatter.format(new Date(blogData.datePublished))}
+            </span>
+          </div>
+        </CardHeader>
+        <CardContent className="flex justify-between gap-2">
+          <div className="flex flex-col gap-3">
+            <CardTitle className="text-xl tracking-normal md:text-2xl md:tracking-tight">
+              {blogData.title}
+            </CardTitle>
+            <CardDescription className="hidden sm:block">
+              {blogData.description}
+            </CardDescription>
+          </div>
+          <img
+            src={blogData.image}
+            className="size-full sm:size-40 object-cover"
+            alt={`thumbnail for ${blogData.title}`}
+          />
+        </CardContent>
+        <CardFooter className="gap-2">
+          <CiHeart className="size-6" />
+          <div className="size-0.5 bg-black dark:bg-white rounded-full mt-0.5" />
+          <span className="text-sm sm:text-base">
+            {likesCountFormatter.format(blogData.likes)}
+          </span>
+        </CardFooter>
+      </Card>
+    </Link>
+  )
+}
+
+function BlogSkeletonCard() {
+  return (
+    <Card className="w-4/5">
+      <CardHeader>
+        <div className="flex items-center gap-2 pb-2">
+          <Skeleton className="size-8 rounded-full aspect-square" />
+          <Skeleton className="size-0.5 rounded-full mt-0.5" />
+          <Skeleton className="h-2 w-52" />
+        </div>
+      </CardHeader>
+      <CardContent className="flex justify-between gap-2">
+        <div className="flex flex-col gap-3 w-4/5">
+          <CardTitle>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="h-2 md:h-5 md:w-[26rem]" />
+              <Skeleton className="h-2 w-1/2 md:hidden" />
+            </div>
+          </CardTitle>
+          <Skeleton className="hidden sm:block h-4 w-full" />
+          <Skeleton className="hidden sm:block h-4 w-full" />
+          <Skeleton className="hidden sm:block h-4 w-1/2" />
+        </div>
+        <Skeleton className="size-20 md:size-40 aspect-square" />
+      </CardContent>
+      <CardFooter className="gap-2">
+        <Skeleton className="size-6 rounded-full" />
+        <Skeleton className="size-0.5 rounded-full mt-0.5" />
+        <Skeleton className="h-2 w-10" />
+      </CardFooter>
+    </Card>
+  )
+}
+
+function InfiniteBlogsSkeleton({ isInitialLoad }: { isInitialLoad?: boolean }) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col w-full h-fit items-center justify-center gap-5 pt-20",
+        {
+          "pt-5": !isInitialLoad,
+        }
+      )}
+    >
+      {Array.from({ length: 10 }, (_, i) => (
+        <BlogSkeletonCard key={i} />
+      ))}
+    </div>
+  )
 }
