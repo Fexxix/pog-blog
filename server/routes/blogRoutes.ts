883136de6348800944ea5ab524af124375634798
Fexxix <fexxix@gmail.com>
2024-04-06 07:41:16 +0500

    add: route for getting comments of a blog and some small changes to "/blogs/:username/:title"

--- server/routes/blogRoutes.ts
+++ server/routes/blogRoutes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express"
 import { BlogModel } from "../models/BlogModel.js"
+import { isAuthenticated } from "../middleware/is-authenticated.js"
+import { CommentModel } from "../models/CommentsModel.js"
+import { UserModel } from "../models/UserModel.js"
 
 export const blogsRouter = Router()
 
@@ -48,24 +51,34 @@ blogsRouter.get("/", async (req, res) => {
   }
 })
 
-blogsRouter.get("/:id", async (req, res) => {
-  const { id } = req.params
+blogsRouter.get("/:username/:title", async (req, res) => {
+  const { username, title } = req.params
+
+  console.log(title)
 
   try {
+    const author = await UserModel.findOne({ username })
+
+    if (!author) {
+      return res.status(400).json({ message: "User does not exist!" })
+    }
+
     const blog = await BlogModel.findOne({
-      _id: id,
+      title,
+      author: author._id,
     })
       .populate({ path: "author", select: "username profilePicture" })
       .exec()
 
     if (blog) {
-      res.json({
+      return res.json({
         id: blog._id,
         title: blog.title,
         content: blog.content,
         description: blog.description,
         datePublished: blog.datePublished.toISOString(),
         likes: blog.likes?.count ?? 0,
+        comments: blog.comments.length,
         author: {
           // @ts-ignore
           username: blog.author.username,
@@ -75,7 +88,65 @@ blogsRouter.get("/:id", async (req, res) => {
         image: blog.image,
       })
     }
+
+    return res.status(404).json({ message: "Blog not found" })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ message: "Internal Server Error!" })
+  }
+})
+
+// blogsRouter.get("/like/:id", isAuthenticated, async (req, res) => {
+//   const { id } = req.params
+
+//   try {
+//     const blog = await BlogModel.updateOne({ _id: id}, {
+//       $inc: {
+//         likes: ""
+//       }
+//     })
+//   } catch {
+//     res.status(500).json({ message: "Internal Server Error!" })
+//   }
+// })
+
+blogsRouter.get("/comments/:id", async (req, res) => {
+  const { id } = req.params
+  const page = parseInt(req.query.page as string)
+
+  if (isNaN(page) || page < 1) {
+    return res.status(400).json({ message: "Invalid page number" })
+  }
+
+  const skip = (page - 1) * PAGE_SIZE
+
+  try {
+    const comments = await CommentModel.find({ blog: id })
+      .skip(skip)
+      .limit(PAGE_SIZE)
+      .populate({ path: "author", select: "username profilePicture" })
+      .exec()
+
+    const totalDocuments = await CommentModel.find({
+      blog: id,
+    }).countDocuments()
+    const hasMore = page * PAGE_SIZE < totalDocuments
+
+    res.json({
+      comments: comments.map((c) => ({
+        content: c.content,
+        author: {
+          // @ts-ignore
+          username: c.author.username,
+          // @ts-ignore
+          profilePicture: c.author.profilePicture,
+        },
+        datePosted: c.datePosted,
+      })),
+      hasMore,
+      nextPage: hasMore ? page + 1 : -1,
+    })
   } catch {
-    res.status(404).json({ message: "Blog not found" })
+    return res.status(500).json({ message: "Internal Server Error!" })
   }
 })
