6797dea05ddd189598048386c515b1162406af70
Fexxix <fexxix@gmail.com>
2024-04-06 07:38:09 +0500

    refactor: moved formatters to utils

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const publicDateFormatter = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+})
+
+export const likesAndCommentsCountFormatter = new Intl.NumberFormat("en", {
+  notation: "compact",
+  compactDisplay: "short",
+})
