c713bd111a87608958813f734f7f3a6eb25db801
Fexxix <fexxix@gmail.com>
2024-04-13 10:03:15 +0500

    fix: rearranged routes so they're properly matched

--- server/routes/blogRoutes.ts
+++ server/routes/blogRoutes.ts
@@ -3,7 +3,6 @@ import { BlogModel } from "../models/BlogModel.js"
 import { isAuthenticated } from "../middleware/is-authenticated.js"
 import { CommentModel } from "../models/CommentsModel.js"
 import { UserModel } from "../models/UserModel.js"
-import { Error } from "mongoose"
 
 export const blogsRouter = Router()
 
@@ -47,10 +46,111 @@ blogsRouter.get("/", async (req, res) => {
           blog.likes.includes(res.locals.session?.userId ?? "") ?? false,
       })),
       hasMore,
-      nextPage: hasMore ? page + 1 : -1,
+      nextPage: hasMore ? page + 1 : null,
     })
   } catch {
     res.status(500).json({ message: "Something went wrong!" })
+  }
+})
+
+blogsRouter.post("/like/:id", isAuthenticated, async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const blog = await BlogModel.findOneAndUpdate(
+      { _id: id, likes: { $nin: [res.locals.session?.userId] } },
+      {
+        $addToSet: {
+          likes: res.locals.session?.userId,
+        },
+      },
+      {
+        new: true,
+      }
+    )
+      .select("likes")
+      .exec()
+
+    if (blog) res.status(200).json({ likes: blog.likes.length })
+    else
+      res
+        .status(400)
+        .json({ message: "You have already liked this blog" })
+        .end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Internal Server Error!" })
+  }
+})
+
+blogsRouter.get("/likedBy/:id", async (req, res) => {
+  const { id } = req.params
+  const page = parseInt(req.query.page as string)
+
+  if (isNaN(page) || page < 1) {
+    return res.status(400).json({ message: "Invalid page number" })
+  }
+
+  const skip = (page - 1) * PAGE_SIZE
+
+  try {
+    const blog = await BlogModel.findOne({ _id: id })
+      .slice("likes", [skip, PAGE_SIZE + 1])
+      .populate("likes", "username profilePicture")
+      .exec()
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" })
+    }
+
+    const hasMore = blog.likes.length > PAGE_SIZE
+    const nextPage = hasMore ? page + 1 : null
+
+    res.json({ users: blog.likes, hasMore, nextPage })
+  } catch (error) {
+    console.error("Error fetching likedBy data:", error)
+    res.status(500).json({ message: "Internal Server Error" })
+  }
+})
+
+blogsRouter.get("/comments/:id", async (req, res) => {
+  const { id } = req.params
+  const page = parseInt(req.query.page as string)
+
+  if (isNaN(page) || page < 1) {
+    return res.status(400).json({ message: "Invalid page number" })
+  }
+
+  const skip = (page - 1) * PAGE_SIZE
+
+  try {
+    const comments = await CommentModel.find({ blog: id })
+      .skip(skip)
+      .limit(PAGE_SIZE)
+      .populate({ path: "author", select: "username profilePicture" })
+      .exec()
+
+    const totalDocuments = await CommentModel.find({
+      blog: id,
+    }).countDocuments()
+    const hasMore = page * PAGE_SIZE < totalDocuments
+
+    res.json({
+      comments: comments.map((c) => ({
+        content: c.content,
+        author: {
+          // @ts-ignore
+          username: c.author.username,
+          // @ts-ignore
+          profilePicture: c.author.profilePicture,
+        },
+        datePosted: c.datePosted,
+      })),
+      hasMore,
+      nextPage: hasMore ? page + 1 : null,
+    })
+  } catch {
+    return res.status(500).json({ message: "Internal Server Error!" })
   }
 })
 
@@ -96,76 +196,5 @@ blogsRouter.get("/:username/:title", async (req, res) => {
   } catch (e) {
     console.error(e)
     res.status(500).json({ message: "Internal Server Error!" })
-  }
-})
-
-blogsRouter.post("/like/:id", isAuthenticated, async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const blog = await BlogModel.findOneAndUpdate(
-      { _id: id, likes: { $nin: [res.locals.session?.userId] } },
-      {
-        $addToSet: {
-          likes: res.locals.session?.userId,
-        },
-      },
-      {
-        new: true,
-      }
-    )
-      .select("likes")
-      .exec()
-
-    if (blog) res.status(200).json({ likes: blog.likes.length })
-    else
-      res
-        .status(400)
-        .json({ message: "You have already liked this blog" })
-        .end()
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: "Internal Server Error!" })
-  }
-})
-
-blogsRouter.get("/comments/:id", async (req, res) => {
-  const { id } = req.params
-  const page = parseInt(req.query.page as string)
-
-  if (isNaN(page) || page < 1) {
-    return res.status(400).json({ message: "Invalid page number" })
-  }
-
-  const skip = (page - 1) * PAGE_SIZE
-
-  try {
-    const comments = await CommentModel.find({ blog: id })
-      .skip(skip)
-      .limit(PAGE_SIZE)
-      .populate({ path: "author", select: "username profilePicture" })
-      .exec()
-
-    const totalDocuments = await CommentModel.find({
-      blog: id,
-    }).countDocuments()
-    const hasMore = page * PAGE_SIZE < totalDocuments
-
-    res.json({
-      comments: comments.map((c) => ({
-        content: c.content,
-        author: {
-          // @ts-ignore
-          username: c.author.username,
-          // @ts-ignore
-          profilePicture: c.author.profilePicture,
-        },
-        datePosted: c.datePosted,
-      })),
-      hasMore,
-      nextPage: hasMore ? page + 1 : -1,
-    })
-  } catch {
-    return res.status(500).json({ message: "Internal Server Error!" })
   }
 })
