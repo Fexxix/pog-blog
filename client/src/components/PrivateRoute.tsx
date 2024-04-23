0946fb0d4c3437c2a3470568b8ca0cb3734daf13
Fexxix <fexxix@gmail.com>
2024-04-23 23:05:39 +0500

    modify: redirection now displays a toast message and a redirectTo state is passed to the navigate call

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,14 +1,17 @@
 import { useEffect, type PropsWithChildren } from "react"
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { useAuthContext } from "../contexts/AuthContextProvider"
+import { toast } from "sonner"
 
 export function PrivateRoute({ children }: PropsWithChildren) {
   const navigate = useNavigate()
   const { user } = useAuthContext()
+  const { pathname } = useLocation()
 
   useEffect(() => {
     if (!user) {
-      navigate("/login")
+      toast("Please login first")
+      navigate("/login", { state: { redirectTo: pathname } })
     }
   }, [])
 
