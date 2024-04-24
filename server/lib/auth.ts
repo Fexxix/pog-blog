7425a1fc6ef7543693dbbaee1e8c8219fc7e79f7
Fexxix <fexxix@gmail.com>
2024-04-24 14:33:44 +0500

    yea...forgot this

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -12,8 +12,6 @@ export const lucia = new Lucia(adapter, {
   sessionCookie: {
     attributes: {
       secure: process.env.NODE_ENV === "production",
-      // @ts-ignore
-      sameSite: "none",
     },
   },
   sessionExpiresIn: new TimeSpan(2, "w"),
