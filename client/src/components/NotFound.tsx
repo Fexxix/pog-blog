98600d7d05c53147a2651c38757371d522ee194d
Fexxix <fexxix@gmail.com>
2024-04-27 11:01:03 +0500

    add: NotFound component

---
+++ client/src/components/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center h-screen">
+      <p className="text-9xl">404</p>
+      <h1 className="text-3xl font-bold">Not Found</h1>
+    </div>
+  )
+}
