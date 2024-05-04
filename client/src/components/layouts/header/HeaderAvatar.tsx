f014f39a42f519f981bfe6e48bb87922b4677712
Fexxix <fexxix@gmail.com>
2024-05-04 10:16:45 +0500

    feat: added signout functionality

--- client/src/components/layouts/header/HeaderAvatar.tsx
+++ client/src/components/layouts/header/HeaderAvatar.tsx
@@ -8,11 +8,48 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { SignOutIcon, UserProfileCircle } from "@/lib/icons"
+import { useMutation } from "@tanstack/react-query"
+import axios, { AxiosError } from "axios"
+import { toast } from "sonner"
+
+function useLogoutMutation() {
+  const navigate = useNavigate()
+  const { setHasSession, setUser } = useAuthContext()
+
+  return useMutation<any, AxiosError | Error>({
+    mutationKey: ["logout"],
+    mutationFn: async () => {
+      return await axios.post(`/api/users/logout`)
+    },
+    onMutate: () => {
+      toast.loading("Logging out...")
+    },
+    onSuccess: () => {
+      setHasSession(false)
+      setUser(null)
+
+      toast.dismiss()
+      toast.success("Logged out!")
+      navigate("/login")
+    },
+    onError: (error) => {
+      const msg =
+        error instanceof AxiosError
+          ? error.response?.data.message
+          : error.message
+
+      toast.dismiss()
+      toast.error(msg)
+    },
+  })
+}
 
 export function HeaderAvatar() {
   const { user } = useAuthContext()
+  const logoutMutation = useLogoutMutation()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -30,7 +67,12 @@ export function HeaderAvatar() {
             Profile
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem className="flex items-center gap-2 cursor-pointer">
+        <DropdownMenuItem
+          onClick={() => {
+            logoutMutation.mutate()
+          }}
+          className="flex items-center gap-2 cursor-pointer"
+        >
           <SignOutIcon /> Sign out
         </DropdownMenuItem>
       </DropdownMenuContent>
